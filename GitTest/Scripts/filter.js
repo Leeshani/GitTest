c36c4f67731b34dc86e36570f04066b4a7dd52bf
Leeshani <leesha.sliit@gmail.com>
2016-12-18 14:14:22 +0530

    customized filter

---
+++ GitTest/Scripts/filter.js
@@ -0,0 +1,16 @@
+﻿mainApp.filter("gender", function () {
+    return function (gender)
+    {
+        switch (gender)
+        {
+            case 1:
+                return "male";
+            case 2:
+                return "female";
+            case 3:
+                return "unknown";
+
+        }
+
+    }
+});
