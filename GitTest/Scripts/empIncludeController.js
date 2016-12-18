03d38d617756a4de56473959ac7c1cc1e426bdc5
Leeshani <leesha.sliit@gmail.com>
2016-12-18 22:30:53 +0530

    added rest +angularJs

---
+++ GitTest/Scripts/empIncludeController.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+var mainApp = angular.module("mainApp", []);
+mainApp.controller("empController", function ($scope) {
+    var employees = [
+                   { name: "Ben", gender: "Male", salary: 55000 },
+                   { name: "Leesha", gender: "Female", salary: 68000 },
+                   { name: "Mark", gender: "Male", salary: 57000 },
+                   { name: "Pam", gender: "Female", salary: 53000 },
+                   { name: "Todd", gender: "Male", salary: 60000 }
+    ];
+
+    $scope.employees = employees;
+    $scope.employeeView = "empTable.html";
+});
---
+++ GitTest/Scripts/employeeRest.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="angular.min.js" />
+
+
+var app = angular
+        .module("myModule", [])
+        .controller("myController", function ($scope, $http) {
+
+            $http.get("http://localhost:16683/api/Employee")
+                 .then(function (response) {
+                     $scope.employees = response.data;
+                 });
+        });
